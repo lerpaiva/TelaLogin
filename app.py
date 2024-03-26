20958a897bbb09b59a585b389dd44efc4c6c6281
lerpaiva <leticiarpaival@gmail.com>
2024-03-26 11:00:33 -0300

    upp

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def login():
+    conexao = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="cadastros"
+)
+    if request.method == "POST":
+        email = request.form['email']
+        senha = request.form['senha']
+
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM cad WHERE email = %s AND senha = %s", (email, senha))
+
+        resultados = cursor.fetchall()
+
+        if resultados:
+            cursor.close()
+            conexao.close()
+            return redirect(url_for('home'))  # Redireciona para a rota /home após autenticação bem-sucedida
+        else:
+            cursor.close()
+            conexao.close()
+            return render_template('forms.html', logado=False)  # Retorna a página de login com mensagem de erro
+    return render_template("forms.html", logado=False)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
